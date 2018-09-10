d9d529a7e1dfa86919813d718c602f9f36590350
Alvaro Arregui <alvaro.arregui@wizeline.com>
2018-09-10 10:46:46 -0500

    Add missing redis configuration

--- lumen/config/database.php
+++ lumen/config/database.php
@@ -3,6 +3,16 @@
 return [
     'default' => env('DB_CONNECTION', 'mongodb'),
 
+    'redis' => [
+        'client' => 'predis',
+        'default' => [
+          'host' => env('REDIS_HOST', '127.0.0.1'),
+          'password' => env('REDIS_PASSWORD', null),
+          'port' => env('REDIS_PORT', 6379),
+          'database' => 0
+        ]
+    ],
+
     'connections' => [
         'mongodb' => [
             'driver'   => 'mongodb',
